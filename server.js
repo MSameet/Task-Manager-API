4244e8727d0f171a2bfcdfb0760fba79cfc08152
Muhammad Sameet Ul Hassan <89504995+MSameet@users.noreply.github.com>
2022-12-05 19:43:33 +0500

    1st commit

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const taskRouter = require("./src/routers/task");
 const port = 3000;
 
 app.use(express.json());
+app.use("", (req, res) => {
+  res.send("app is working");
+});
 app.use(userRouter);
 app.use(taskRouter);
 
